d4b4267e6ce1fe8da9442764522664be976e24f5
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-03-30 23:50:53 +0000

    Kehbob Van

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+
+files {
+	'data/**/handling.meta',
+	'data/**/vehicles.meta',
+	'data/**/carvariations.meta',
+}
+
+
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
